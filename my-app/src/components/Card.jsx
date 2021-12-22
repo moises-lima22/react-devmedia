8025786ce2298b0d7b58f50c97fb728996409b3e
Moises Silva <moises.silva2015@outlook.com>
2021-12-21 21:13:39 -0300

    add img resultado

--- my-app/src/components/Card.jsx
+++ my-app/src/components/Card.jsx
@@ -10,6 +10,7 @@ export default function Card() {
         Facilita em manter partes menores funcionando corretamente.
       </p>
       <p>Você pode reutilizá-los, ou seja, menos código para se escrever!</p>
+
     </div>
   );
 }
